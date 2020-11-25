e0fafdd94b98730c9341df48109ff40734c8c00b
Tim D'Arcy <trdarcy@outlook.com>
2020-11-25 20:21:10 +1000

    Added bulma, basic intro section and project switch functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Projects from './Components/Projects'
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="welcome-content mt-5">
+          <h1>Hello World</h1>
+          <h2>Welcome to by portfolio site.  Please see below some client side examples of React. </h2>
+        </div>
+        <Projects/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+class Projects extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            currencyCalculator: false,
+            keyMapper: false,
+            workFlowBoard: false
+        }
+    }
+    handleCurrencyCalulator = () => {
+        this.setState({
+            currencyCalculator: true,
+            keyMapper: false,
+            workFlowBoard: false
+        })
+    }
+
+    handleKeyMapper = () => {
+        this.setState({
+            currencyCalculator: false,
+            keyMapper: true,
+            workFlowBoard: false
+        })
+    }
+
+    handleWorkFlowBoard = () => {
+        this.setState({
+            currencyCalculator: false,
+            keyMapper: false,
+            workFlowBoard: true
+        })
+    }
+    displayActiveProject = () => {
+        if (this.state.currencyCalculator){
+            return <h1>CurrencyCalculator On</h1>
+        }
+        else if(this.state.keyMapper){
+            return <h1>KeyMapper On</h1>
+        }
+        else if(this.state.workFlowBoard){
+            return <h1>WorkFlowBoard On</h1>
+        }
+        else{
+            return <h1>Nothing selected</h1>
+        }
+    }
+
+    render(){
+        return(
+        <>
+            <div className="tabs is-centered">
+                <ul>
+                    <li className={this.state.currencyCalculator ? "is-active" : ""} onClick={this.handleCurrencyCalulator}><a>Currency Calculator</a></li>
+                    <li className={this.state.keyMapper ? "is-active" : ""} onClick={this.handleKeyMapper}><a>Key Mapper</a></li>
+                    <li className={this.state.workFlowBoard ? "is-active" : ""} onClick={this.handleWorkFlowBoard}><a>Work Flow Board</a></li>
+                </ul>
+            </div>
+
+            <div className="is-centered">
+                {this.displayActiveProject()}
+            </div>
+            
+        </>
+        )
+    }
+}
+
+export default Projects;
